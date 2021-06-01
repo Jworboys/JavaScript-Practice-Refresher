51af98bd92860e0ceb14b2540258c459d91afa0d
jworboys <Jordworb@gmail.com>
2021-06-01 12:31:32 -0400

    Adding a coding Challenge

--- Coding_Challenges/script.js
+++ Coding_Challenges/script.js
@@ -140,3 +140,49 @@ for(const [team, odd] of Object.entries(game.odds)){
     const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
     console.log(`Odd of ${teamStr} ${odd}`);
 }
+
+
+/*1. Create an array 'events' of the different game events that happened (no 
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on 
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking 
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+GOOD LUCK */
+
+const gameEvents = new Map([
+    [17, '⚽ GOAL'],
+    [36, '� Substitution'],
+    [47, '⚽ GOAL'],
+    [61, '� Substitution'],
+    [64, '� Yellow card'],
+    [69, '� Red card'],
+    [70, '� Substitution'],
+    [72, '� Substitution'],
+    [76, '⚽ GOAL'],
+    [80, '⚽ GOAL'],
+    [92, '� Yellow card'],
+]);
+
+//1.
+console.log(gameEvents.values);
+const indGameEvents = [...new Set(gameEvents.values())];
+console.log(indGameEvents);
+
+
+//2.
+gameEvents.delete(64);
+
+//3.
+const time = [...gameEvents.keys()].pop();
+console.log(`An event happened, on 
+average, every ${time / gameEvents.size} minutes`);
+
+//4.
+for(const [min, event] of gameEvents){
+    const half = min <= 45 ? 'FIRST' : 'SECOND';
+    console.log(`[${half} HALF] ${min}: ${event}`);
+}
