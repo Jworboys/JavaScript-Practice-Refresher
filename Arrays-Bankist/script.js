90d2b81f0f2697f590bbdbd38a2f764d8cca4edf
jworboys <Jordworb@gmail.com>
2021-06-11 17:21:00 -0400

    Adding rounding, and cleaning up decimals

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -74,7 +74,7 @@ const displayMovements = function(movements, sort = false){
     const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value">${mov}</div>
+    <div class="movements__value">${mov.toFixed(2)}</div>
     </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -87,7 +87,7 @@ const displayMovements = function(movements, sort = false){
 // Used to display the balance
 const calcDisplayBalance = function(acc){
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
   
 }
 
@@ -95,15 +95,15 @@ const calcDisplayBalance = function(acc){
 
 const calcDisplaySummary = function(acc){
   const incomes = acc.movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${out}€`;
+  labelSumOut.textContent = `${out.toFixed(2)}€`;
 
   const intrest = acc.movements.filter(mov => mov > 0).map(deposit => (deposit * acc.interestRate)/100).filter((int, i, arr) =>{
     return int >= 1;
   }).reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${intrest}€`;
+  labelSumInterest.textContent = `${intrest.toFixed(2)}€`;
 }
 
 
@@ -177,7 +177,8 @@ btnTransfer.addEventListener('click', function(e){
 btnLoan.addEventListener('click', function(e){
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
+
   if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)){
     // Add the movement
     currentAccount.movements.push(amount);
